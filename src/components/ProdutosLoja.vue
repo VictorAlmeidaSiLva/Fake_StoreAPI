50f15425ffaeab21c57cffd33a11e7ea8cfbdd0f
Victor Augusto de Almeida Silva <victoraugustoas41@gmail.com>
2022-05-23 16:46:28 -0300

    Spinner colocado na Home

--- src/components/ProdutosLoja.vue
+++ src/components/ProdutosLoja.vue
@@ -1,10 +1,16 @@
 <template>
-  <div v-if="products">
-    <div v-for="item in products" :key="item.id" :value="item.id">
-      <li>{{ item.title }} {{ item.price }}</li>
-      <img :src="item.image" alt="" />
+  <div>
+    <div v-if="products">
+      <div v-for="item in products" :key="item.id" :value="item.id">
+        <li>{{ item.title }} {{ item.price }}</li>
+        <img :src="item.image" alt="" />
+      </div>
     </div>
-    
+    <div v-else> <div class="d-flex justify-content-center " >
+  <div class="spinner-border" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</div> </div>
   </div>
 </template>
 
@@ -16,8 +22,7 @@ const store = useProductsStore();
 const products = computed(() => store.products);
 
 onMounted(() => {
-  store.listaProducts()
-  
+  store.listaProducts();
 });
 </script>
 
